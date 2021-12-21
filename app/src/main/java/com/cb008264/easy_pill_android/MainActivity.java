d00054386f37841305220316ea523a96cc820f50
Raaid <raaidrizvi123@gmail.com>
2021-12-21 12:18:10 +0530

    first commit

---
+++ app/src/main/java/com/cb008264/easy_pill_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cb008264.easy_pill_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
